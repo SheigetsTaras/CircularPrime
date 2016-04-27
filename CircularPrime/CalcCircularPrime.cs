4df5b6304347ee2dbae20d6b15f5a1020158745a
TarasSheigets <targan@ukr.net>
2016-04-27 12:54:09 +0300

    Create class CalcCircularPrime
    Add functions: IsPrime, Rotate, FindCircular, PrintResult, Start

---
+++ CircularPrime/CalcCircularPrime.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CircularPrime
+{
+    class CalcCircularPrime
+    {
+        private List<int> _primeList;
+        private Stopwatch _timeAlgorithm;
+
+        private bool IsPrime(int candidate)
+        {
+            if ((candidate & 1) == 0)
+            {
+                return candidate == 2; //return true , because 2 is prime number
+            }
+
+            for (int i = 3; (i * i) <= candidate; i += 2)
+            {
+                if ((candidate % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return candidate != 1;
+        }
+
+
+        private int Rotate(int iNumber)
+        {
+            double givenNo = iNumber;
+            int reminder = (int)iNumber % 10;
+            int quotient = (int)iNumber / 10;
+            return Convert.ToInt32((reminder * Math.Pow(10, iNumber.ToString().Length - 1)) + quotient);
+        }
+
+
+        private void FindCircular(int primeNumber)
+        {
+            var res = primeNumber;
+            List<int> resultList = new List<int>() { primeNumber };
+
+            for (int i = 0; i < primeNumber.ToString().Length - 1; i++)
+            {
+                res = Rotate(res);
+
+                if (IsPrime(res))
+                {
+                    if (!resultList.Contains(res) || res == primeNumber)  //(res == primeNumber - used for checking numbers like 111, 333)
+                        resultList.Add(res);
+                }
+            }
+
+            if (resultList.Count == (primeNumber.ToString().Length))
+            {
+                resultList = resultList.Distinct().ToList();
+                _primeList.AddRange(resultList);
+            }
+        }
+
+
+        public void PrintResult()
+        {
+            Console.Write("Circular Prime numbers: ");
+            foreach (var pr in _primeList)
+            {
+                Console.Write(pr + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Total Circular Prime numbers: " + _primeList.Count);
+            Console.WriteLine("Elapsed time " + _timeAlgorithm.Elapsed.TotalSeconds);
+        }
+
+
+        public bool Start(int value)
+        {
+            if (value > 0)
+            {
+                _primeList = new List<int>();
+                _timeAlgorithm = Stopwatch.StartNew();
+                for (int i = 0; i < value; i++)
+                {
+                    if (!_primeList.Contains(i) && IsPrime(i))
+                    {
+                        FindCircular(i);
+                    }
+                }
+                _timeAlgorithm.Stop();
+
+                _primeList.Sort();
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("The number must be greater than zero!");
+                return false;
+            }
+          
+
+        }
+    }
+}
---
+++ CircularPrime/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CircularPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 0;
+           
+            try
+            {
+                if (args.Length != 0)
+                {
+                    value = Convert.ToInt32(args[0]);
+                }
+                else
+                {
+                    value = Convert.ToInt32(Console.ReadLine());
+                }
+                CalcCircularPrime calcCircularPrime = new CalcCircularPrime();
+                if(calcCircularPrime.Start(value))
+                    calcCircularPrime.PrintResult();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
